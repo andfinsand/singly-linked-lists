3074703ab2a67f18b67b7a6c8feae8272bc19215
Andrew Finsand <andfinsand@gmail.com>
2022-10-17 16:48:11 -0700

    Fixed merge conflicts.

--- singly_linked_lists.py
+++ singly_linked_lists.py
@@ -6,19 +6,31 @@ class Node:
 class SLL:
     def __init__(self):
         self.head = None
-        
+
+    # add node to front of array
+
+    def add_to_front(self, key):
+        new_node = Node(key) # initialize new node from Node class
+        current_head = self.head # create new variable to store current head (temp)
+        new_node.next = current_head # direct the new nodes 'next' pointer to existing head (stored in new variable)
+        self.head = new_node # reassign new node as head
+        return self
+
     # add value to end of array
-    
-    def addBack(self, value):
-        if self.head == None:
-            self.head = Node(value)
+
+    def add_to_back(self, key):
+        if self.head == None: # check if head is None
+            self.head = Node(key)
         else:
             runner = self.head
             while runner.next != None:
                 runner = runner.next
-            runner.next = Node(value)
+            runner.next = Node(key)
         return self
-    def displayValues(self):
+
+    # traverse through array
+
+    def display_values(self):
         if self.head == None:
             print("No values")
         else:
@@ -45,8 +57,8 @@ class SLL:
 x = SLL()
 x.head = Node(4)
 
-x.addBack(3).addBack(5)
+x.add_to_front(3).add_to_back(5)
 
-x.displayValues()
+x.display_values()
 
-x.maxValue()
+x.max_value()
